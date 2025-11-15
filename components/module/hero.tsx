c0421938297b3d64f2f26765fddaa19604cb186c
ILYESS24 <ilyessbenina3@gmail.com>
2025-11-16 00:19:50 +0100

    Remove Cube3D completely

--- components/module/hero.tsx
+++ components/module/hero.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
                     <div className="absolute top-1/4 left-1/2 -z-10 gradient w-3/4 -translate-x-1/2 h-1/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
 
                     <div className="rounded-lg lg:rounded-[24px] border p-2 border-neutral-700 bg-black h-[500px] md:h-[600px]">
-                        <Cube3D />
+                        <Image src="/images/dashboard.png" alt="dashboard" width={1920} height={1080} className="rounded-lg lg:rounded-[20px]" />
                     </div>
                 </div>
             </Container>
