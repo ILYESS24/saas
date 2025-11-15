0bdf9fee0764d74801024bb8142e23488043b94e
ILYESS24 <ilyessbenina3@gmail.com>
2025-11-15 23:24:40 +0100

    Replace dashboard image with 3D cube animation in hero section

--- components/module/hero.tsx
+++ components/module/hero.tsx
@@ -1,8 +1,133 @@
-import Image from "next/image";
+"use client";
+
 import { ArrowRightIcon } from "lucide-react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Geometry, Base, Subtraction} from '@react-three/csg'
+// @ts-ignore
+import { RoundedBoxGeometry } from "three/examples/jsm/geometries/RoundedBoxGeometry.js";
+import { Bloom, N8AO, SMAA, EffectComposer } from '@react-three/postprocessing'
+import { useRef } from "react";
+import { Mesh } from "three";
+import { KernelSize } from "postprocessing";
 
 import { BlurText } from "@/components/ui/blur-text";
 import Container from "@/components/global/container";
+
+function Shape() {
+  const meshRef = useRef<Mesh>(null);
+  const innerSphereRef = useRef<Mesh>(null);
+
+  useFrame((_, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta * 0.5;
+      meshRef.current.rotation.y += delta * 0.3;
+      meshRef.current.rotation.z += delta * 0.2;
+    }
+    if (innerSphereRef.current) {
+      innerSphereRef.current.rotation.x += delta * 0.3;
+      innerSphereRef.current.rotation.y += delta * 0.5;
+      innerSphereRef.current.rotation.z += delta * 0.1;
+    }
+  });
+
+  return (
+    <>
+      <mesh ref={meshRef}>
+        <meshPhysicalMaterial 
+          roughness={0}
+          metalness={0.95}
+          clearcoat={1}
+          clearcoatRoughness={0.1}
+          color="#000000"
+        />
+
+        <Geometry>
+          <Base>
+            <primitive
+              object={new RoundedBoxGeometry(2, 2, 2, 7, 0.2)}
+            />
+          </Base>
+
+          <Subtraction>
+            <sphereGeometry args={[1.25, 64, 64]} />
+          </Subtraction>
+        </Geometry>
+      </mesh>
+      
+      <mesh ref={innerSphereRef}>
+        <sphereGeometry args={[0.8, 32, 32]} />
+        <meshPhysicalMaterial 
+          color="#ffffff"
+          emissive={"white"}
+          emissiveIntensity={1}
+        />
+      </mesh>
+    </>
+  );
+}
+
+function Environment() {
+  return (
+    <>
+      <directionalLight 
+        position={[-5, 5, -5]} 
+        intensity={0.2} 
+        color="#e6f3ff"
+      />
+      <directionalLight 
+        position={[0, -5, 10]} 
+        intensity={0.4} 
+        color="#fff5e6"
+      />
+      <ambientLight intensity={0.8} color="#404040" />
+      <pointLight 
+        position={[8, 3, 8]} 
+        intensity={0.2} 
+        color="#ffeecc"
+        distance={20}
+      />
+      <pointLight 
+        position={[-8, 3, -8]} 
+        intensity={0.2} 
+        color="#ccf0ff"
+        distance={20}
+      />
+      <directionalLight 
+        position={[0, -10, 0]} 
+        intensity={0.2} 
+        color="#f0f0f0"
+      />
+    </>
+  );
+}
+
+function CubeScene() {
+  return (
+    <Canvas
+      className="w-full h-full"
+      camera={{ position: [5, 5, 5], fov: 50 }}
+    >
+      <Environment />
+      <Shape />
+      <EffectComposer multisampling={0}>
+        <N8AO halfRes color="black" aoRadius={2} intensity={1} aoSamples={6} denoiseSamples={4} />
+        <Bloom
+          kernelSize={3}
+          luminanceThreshold={0}
+          luminanceSmoothing={0.4}
+          intensity={0.6}
+        />
+        <Bloom
+          kernelSize={KernelSize.HUGE}
+          luminanceThreshold={0}
+          luminanceSmoothing={0}
+          intensity={0.5}
+        />
+        <SMAA />
+      </EffectComposer>
+    </Canvas>
+  );
+}
 
 const Hero = () => {
     return (
@@ -38,14 +163,8 @@ const Hero = () => {
                 <div className="relative mx-auto max-w-7xl rounded-xl lg:rounded-[32px] border border-neutral-200/50 p-2 backdrop-blur-lg border-neutral-700 bg-neutral-800/50 md:p-4 mt-12">
                     <div className="absolute top-1/4 left-1/2 -z-10 gradient w-3/4 -translate-x-1/2 h-1/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
 
-                    <div className="rounded-lg lg:rounded-[24px] border p-2 border-neutral-700 bg-black">
-                        <Image
-                            src="/images/dashboard.png"
-                            alt="dashboard"
-                            width={1920}
-                            height={1080}
-                            className="rounded-lg lg:rounded-[20px]"
-                        />
+                    <div className="rounded-lg lg:rounded-[24px] border p-2 border-neutral-700 bg-black h-[500px] md:h-[600px]">
+                        <CubeScene />
                     </div>
                 </div>
             </Container>
